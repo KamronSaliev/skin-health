da7c4cbdb92bb430882cec52671f0c89ef71741c
Kamron Saliev <kamron.saliev@amayasoft.uz>
2021-03-22 12:58:00 +0500

    Updated main activity to log the successfull load of openCV

--- app/src/main/java/com/example/skinhealth/MainActivity.java
+++ app/src/main/java/com/example/skinhealth/MainActivity.java
@@ -3,8 +3,21 @@ package com.example.skinhealth;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import org.opencv.android.OpenCVLoader;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
+
+    static {
+        if (OpenCVLoader.initDebug()) {
+            Log.d(TAG, "OpenCV loaded successfully!");
+        } else {
+            Log.d(TAG, "OpenCV load failed!");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
