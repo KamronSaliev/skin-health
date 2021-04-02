c4ec5f1c14a9ab2103ee7458b6e457941fc4df31
Kamron Saliev <kamron.saliev@amayasoft.uz>
2021-04-02 16:57:59 +0500

    Removed redundant code

--- app/src/main/java/com/example/skinhealth/MainActivity.java
+++ app/src/main/java/com/example/skinhealth/MainActivity.java
@@ -163,29 +163,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         convertBmp = Bitmap.createBitmap(srcMat.cols(), srcMat.rows(), Bitmap.Config.ARGB_8888);
-        convertBmp = getRoundedCornerBitmap(convertBmp, 50);
         Utils.matToBitmap(srcMat, convertBmp);
         srcMat.release();
         return convertBmp;
-    }
-
-    public Bitmap getRoundedCornerBitmap(Bitmap bitmap, int pixels) {
-        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
-        Canvas canvas = new Canvas(output);
-
-        final int color = 0xff424242;
-        final Paint paint = new Paint();
-        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
-        final RectF rectF = new RectF(rect);
-
-        paint.setAntiAlias(true);
-        canvas.drawARGB(0, 0, 0, 0);
-        paint.setColor(color);
-        canvas.drawRoundRect(rectF, (float) pixels, (float) pixels, paint);
-
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
-        canvas.drawBitmap(bitmap, rect, rect, paint);
-
-        return output;
     }
 }
