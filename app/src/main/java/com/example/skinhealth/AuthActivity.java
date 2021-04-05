ad5c963efbeab86a78ac4144f601a9c0a862f42b
Joseph <josephdance1997@gmail.com>
2021-04-05 09:44:40 +0500

    created layout for login activities, added fonts and drawables

--- app/src/main/java/com/example/skinhealth/AuthActivity.java
+++ app/src/main/java/com/example/skinhealth/AuthActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.skinhealth.ui.login.LoginActivity;
 
@@ -16,7 +17,7 @@ public class AuthActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
-        Button logInButton = findViewById(R.id.loginbutton);
+        ImageButton logInButton = findViewById(R.id.loginbutton);
         logInButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 System.out.println("Log In Button Clicked");
